e6fd1f8030279fe5fd34706d7a1974eeb93ac4a7
Daniel Madrid <danimydev@gmail.com>
2022-02-13 09:26:28 -0500

    add book crud test on knex adapter

--- database/adapters/knex.js
+++ database/adapters/knex.js
@@ -13,29 +13,45 @@ class KnexAdapter {
   }
 
   async selectRecord({ table, values }) {
-    return this.#knex
+    const selected = await this.#knex
       .select('*')
       .from(table)
       .where(values);
+    return this.parsedDBRecord(selected[0]);
   }
 
   async insertRecord({ table, values }) {
-    return this.#knex(table)
+    const inserted = await this.#knex(table)
       .insert(values)
       .returning('*');
+    return this.parsedDBRecord(inserted[0]);
   }
 
   async updateRecord({ table, id, values }) {
-    return this.#knex(table)
+    const updated = await this.#knex(table)
       .where({ id })
       .update(values)
       .returning('*');
+    return this.parsedDBRecord(updated[0]);
   }
 
   async deleteRecord({ table, values }) {
     return await this.#knex(table)
       .where(values)
       .del();
+  }
+
+  parsedDBRecord(record) {
+    const parsed = { ...record };
+
+    delete parsed['created_at'];
+    delete parsed['updated_at'];
+
+    if (record.hasOwnProperty('published_date')) {
+      parsed.published_date = new Date(parsed.published_date + '').toISOString();
+    }
+
+    return parsed;
   }
 
   getKnex() {
--- database/adapters/knex.test.js
+++ database/adapters/knex.test.js
@@ -10,14 +10,80 @@ database.password = 'secret';
 database.name = 'test';
 */
 
+const knexAdapter = new KnexAdapter({
+  knex,
+  config: buildKnexConfig(database),
+});
+
 describe('knex ORMAdpater behaviour', () => {
 
   test('should create a KnexAdapter', () => {
-    const knexAdapter = new KnexAdapter({
-      knex,
-      config: buildKnexConfig(database),
-    });
     expect(knexAdapter.config).toEqual(buildKnexConfig(database));
+  });
+
+  test('should insert and delete a record from books', async () => {
+    const inserted = await knexAdapter.insertRecord({
+      table: 'book',
+      values: {
+        id: 'ad88a73e5ebe79e715f1a273d22819b3',
+        title: 'book 1',
+        author_id: 'author 1',
+        published_date: '12-02-2022',
+        category: 'category 1',
+        isbn: 'isbn 1',
+      }
+    });
+    expect(inserted).toEqual({
+      id: 'ad88a73e5ebe79e715f1a273d22819b3',
+      title: 'book 1',
+      author_id: 'author 1',
+      published_date: '2022-12-02T05:00:00.000Z',
+      category: 'category 1',
+      isbn: 'isbn 1',
+    });
+  });
+
+  test('should select a record from books', async () => {
+    const selected = await knexAdapter.selectRecord({
+      table: 'book',
+      values: {
+        id: 'ad88a73e5ebe79e715f1a273d22819b3',
+      }
+    });
+    expect(selected).toEqual({
+      id: 'ad88a73e5ebe79e715f1a273d22819b3',
+      title: 'book 1',
+      author_id: 'author 1',
+      published_date: '2022-12-02T05:00:00.000Z',
+      category: 'category 1',
+      isbn: 'isbn 1',
+    });
+  });
+
+  test('should update a book record from books table', async () => {
+    const updated = await knexAdapter.updateRecord({
+      table: 'book',
+      id: 'ad88a73e5ebe79e715f1a273d22819b3',
+      values: {
+        id: 'ad88a73e5ebe79e715f1a273d22819b3',
+        title: 'book updated',
+        author_id: 'author 1 updated',
+        published_date: '12-02-2022',
+        category: 'category 1',
+        isbn: 'isbn 1',
+      }
+    });
+    expect(updated.title).toBe('book updated');
+  });
+
+  test('should delete a book record from books table', async () => {
+    const deleted = await knexAdapter.deleteRecord({
+      table: 'book',
+      values: {
+        id: 'ad88a73e5ebe79e715f1a273d22819b3',
+      }
+    });
+    expect(!!deleted).toBe(true);
   });
 
 });
